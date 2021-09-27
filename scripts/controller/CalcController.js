32835cf37e518cab73d10d43e75f96d9a8a9e1fe
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-09-27 11:31:42 -0300

    Adiciona manipulação da DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,22 +1,33 @@
 class CalcController {
     constructor() {
         this._displayCalc = "0";
-        this._dataAtual = Date.now();
+        this._currentDate = Date.now();
+        this.initialize();
+    };
+
+    initialize() {
+        let displayCalEl= document.querySelector("#display");
+        let dateEl= document.querySelector("#data");
+        let timeEl= document.querySelector("#hora");
+
+        displayCalEl.innerHTML = "4567"
+        dateEl.innerHTML = "26/09/2021"
+        timeEl.innerHTML = "16:06"
     };
 
     get displayCalc() {
         return this._displayCalc;
     };
 
-    set displayCalc(valor) {
-        this._displayCalc = valor;
+    set displayCalc(value) {
+        this._displayCalc = value;
     };
 
     get dataAtual() {
-        return this._dataAtual.toLocaleString();
+        return this._currentDate.toLocaleString();
     };
 
-    set dataAtual(valor) {
-        this._dataAtual = valor;
+    set dataAtual(value) {
+        this._currentDate = value;
     };
 };
